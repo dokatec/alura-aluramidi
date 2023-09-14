4223ef45343cadcef2775e3e9f9fd7b6281e593e
Jhonatan Pacheco <47837212+dokatec@users.noreply.github.com>
2023-09-14 08:45:50 -0300

    update files

--- main.js
+++ main.js
@@ -1,11 +1,38 @@
+//function para tocar o som
 function tocaSom(idElementAudio) {
   document.querySelector(idElementAudio).play();
 }
 
+//pegando todos elemento com .tecla
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-let contador = 0;
+//usando for
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
 
+  const instrumento = tecla.classList[1];
+
+  const idAudio = `#som_${instrumento}`;
+
+  tecla.onclick = () => {
+    tocaSom(idAudio);
+  };
+
+  tecla.onkeydown = function (evento) {
+    console.log(evento.code);
+
+    if (evento.code === "Space") {
+      tecla.classList.add("ativa");
+    }
+  };
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
+  };
+}
+
+//usando o while
+/*
 while (contador < listaDeTeclas.length) {
   const tecla = listaDeTeclas[contador];
 
@@ -21,3 +48,4 @@ while (contador < listaDeTeclas.length) {
 
   console.log(contador);
 }
+*/
