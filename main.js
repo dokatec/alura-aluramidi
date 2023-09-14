89128cc3896f4ecedb0875910c851e93c8cdde8a
Jhonatan Pacheco <47837212+dokatec@users.noreply.github.com>
2023-09-13 23:07:31 -0300

    update file main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+function tocaSomPom() {
+  document.querySelector("#som_tecla_pom").play();
+}
+
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+let contador = 0;
+
+while (contador < 9) {
+  listaDeTeclas[contador].onclick = tocaSomPom;
+
+  contador = contador + 1;
+
+  console.log(contador);
+}
