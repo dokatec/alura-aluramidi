f87f9f10654b4f42ba332e056b264885c1530ce2
Jhonatan Pacheco da Silva <jhonatan.tec@gmail.com>
2023-09-14 13:03:47 -0300

    update files

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 //function para tocar o som
-function tocaSom(idElementAudio) {
-  document.querySelector(idElementAudio).play();
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+
+  if (elemento != null && elemento.localName === 'audio') {
+    elemento.play();
+  } else {
+    console.log('elemento não encontrado');
+  }
 }
 
 //pegando todos elemento com .tecla
@@ -21,7 +27,7 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
   tecla.onkeydown = function (evento) {
     console.log(evento.code);
 
-    if (evento.code === "Space") {
+    if (evento.code === "Space" || evento.code === 'Enter') {
       tecla.classList.add("ativa");
     }
   };
